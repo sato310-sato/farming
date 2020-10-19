61b331e1dcb77f556dc6d12527a9e1aca48f05aa
sato310-sato <under_color27@yahoo.co.jp>
2020-10-19 21:59:03 +0900

    userのバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :tweets
+  has_many :comments
+
+  validates :name, presence: true 
+  
+  @password_regex = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,100}+\z/i
+  validates :password, presence: true
+  validates_format_of :password, with: @password_regex
+  validates_length_of :password, minimum: 6
+
+  @email_regex = /\A\S+@\S+\.\S+\z/
+  validates :email, uniqueness: true
+  validates_format_of :email, with: @email_regular_expressions, allow_nil: true
+
 end
